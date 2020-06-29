28b7cf085f5cf3bdfa1b2511199a2f44cee1c524
David Pacheco <dap@oxidecomputer.com>
2020-06-29 13:25:30 -0700

    simplify test case

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 fn main() {
-    let _matches = clap::App::new("demo")
+    clap::App::new("demo")
         .arg(clap::Arg::with_name("foo").required(true))
         .get_matches();
-
-    println!("{}\n", std::fs::read_to_string("help_file").unwrap());
-
-    println!("{}", include_str!("file.txt"));
 }
