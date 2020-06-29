bf351038e2296fc2584db31069191c83408a1093
David Pacheco <dap@oxidecomputer.com>
2020-06-29 12:57:23 -0700

    see where carriage returns are being removed

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ fn main() {
         .arg(clap::Arg::with_name("foo"))
         .get_matches();
 
+    println!("{}\n", std::fs::read_to_string("help_file").unwrap());
+
     println!("{}", include_str!("file.txt"));
 }
